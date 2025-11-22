5548b2e8160095e7da15e623d2be93d2f9ea0a65
Pedro Cavalheiro <pdrocavalheiro@gmail.com>
2025-11-22 13:28:31 -0300

    Update logo SVG and remove unused vectors config
    Replaces the existing logo-se-white.svg with a new, simplified version and deletes the unused lib/core/configs/assets/vectors.dart file.

--- lib/core/configs/assets/vectors.dart
+++
@@ -1,9 +0,0 @@
-class Vectors {
-  static const String basePath = 'assets/svg/';
-  static const String format = '.svg';
-
-  static const String logo = '${basePath}logo-poliedro-se$format';
-  static const String whiteLogo = '${basePath}logo-se-white$format';
-  static const String topPattern = '${basePath}top-pattern$format';
-  static const String bottomPattern = '${basePath}bottom-pattern$format';
-}
